f9fd84d757b4166b5efac6e4ebb1395e400084ad
Anudit Nagar <nagaranudit@gmail.com>
2024-10-23 18:55:06 +0530

    Cleanups

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,11 +21,11 @@ export default function Layout({ children }: { children: ReactNode }) {
         <meta httpEquiv="Content-Language" content="en" />
         <meta
           name="description"
-          content="Make beautiful websites with Next.js & MDX."
+          content="Documentation to build on the Omnid.io Ecosystem."
         />
         <meta
           name="og:description"
-          content="Make beautiful websites with Next.js & MDX."
+          content="Documentation to build on the Omnid.io Ecosystem."
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content="/og.jpg" />
